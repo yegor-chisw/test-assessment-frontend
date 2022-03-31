aea62c942e3b4c8e0d5733939a771056a8daa0cc
yegor-chisw <yegor.tsukanov@chisw.com>
2022-04-01 01:06:41 +0300

    Add getRequest hook

---
+++ src/hooks/useGetRequest.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useCallback } from 'react';
+
+const API_URL = 'http://localhost:8080';
+
+export const useGetRequest = (path) => {
+    const [isLoading, setIsloading] = useState(false);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    const getData = useCallback(async () => {
+        try {
+            setIsloading(true);
+            setTimeout( async () => {
+                // const response = await fetch(`${API_URL}/${path}`);
+                // const json = await response.json();
+                // setData(json);
+                setIsloading(false);
+            }, 1000)
+        } catch (error) {
+            setError(error);
+            setIsloading(false);
+        }
+    }, [path]);
+
+    useEffect(() => {
+        getData();
+    }, [getData]);
+
+    return [data, isLoading, error];
+};
